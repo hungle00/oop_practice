d65ab509b76f484e1339ea424fe47ec08cadd227
hungle00 <hungle0903>
2020-10-12 09:43:23 +0700

    Refactor hanoi's tower

--- src/tower_of_hanoi/Towers.java
+++ src/tower_of_hanoi/Towers.java
@@ -7,68 +7,31 @@ public class Towers {
 	static public Stack<Integer> A = new Stack<Integer>();
 	static public Stack<Integer> B = new Stack<Integer>();
 	static public Stack<Integer> C = new Stack<Integer>();
+	static int count = -1;
 
-	public static void solve2DiscsTOH(Stack<Integer> source,
-									Stack<Integer> temp,
-									Stack<Integer> dest) {            
-		temp.push(source.pop());
-		printStacks();
-		dest.push(source.pop());
-		printStacks();
-		dest.push(temp.pop());
-		printStacks();
+	public static void solveTOH(int nums, Stack<Integer> source,
+	 									Stack<Integer> temp,
+	 									Stack<Integer> dest) {
+		if(nums >= 1) {
+			solveTOH(nums - 1, source, dest, temp);
+			moveDisk(source, dest);
+			printStacks();
+			solveTOH(nums - 1, temp, source, dest);
+		}
 	}
 
-	static public int solveTOH(int nDiscs, Stack<Integer> source,
-										Stack<Integer> temp,
-										Stack<Integer> dest) {
-		if (nDiscs <= 4) {
-			if ((nDiscs % 2) == 0) { //if pair number of disks
-				solve2DiscsTOH(source, temp, dest);
-				nDiscs = nDiscs - 1;
-				if (nDiscs == 1)
-					return 1;
-
-				temp.push(source.pop());
-				printStacks();
-				//new source is dest, new temp is source, new dest is temp;
-				solve2DiscsTOH(dest, source, temp);
-				dest.push(source.pop());
-				printStacks();
-				//new source is temp, new temp is source, new dest is dest;
-				solveTOH(nDiscs, temp, source, dest);
-			}
-			else {// if impair number of disks
-				if (nDiscs == 1)
-					return -1;
-				solve2DiscsTOH(source, dest, temp);
-				nDiscs = nDiscs - 1;
-				dest.push(source.pop());
-				printStacks();
-				solve2DiscsTOH(temp, source, dest);
-			}
-			return 1;
-		}
-		else if (nDiscs >= 5) {
-			solveTOH(nDiscs - 2, source, temp, dest);
-			temp.push(source.pop());
-			printStacks();
-			solveTOH(nDiscs - 2, dest, source, temp);
-			dest.push(source.pop());
-			printStacks();
-			solveTOH(nDiscs - 1, temp, source, dest);
-		}
-		return 1;
+	public static void moveDisk(Stack<Integer> source, Stack<Integer> dest) {
+		dest.push(source.pop());
 	}
 
 	static public void printStacks(){
-		System.out.println();
 		printStack(A);
 		System.out.print(" , ");
 		printStack(B);
 		System.out.print(" , ");
 		printStack(C);
-
+		count ++;
+		System.out.println();
 	}
 
 	public static void printStack(Stack<Integer> s) {
@@ -80,8 +43,8 @@ public class Towers {
 		System.out.print("\nEnter the number of discs: ");
 
 		int discs = input.nextInt();
-		if (discs <= 1 || discs >= 10) {
-			System.out.println("Please enter between 2 - 12");
+		if (discs < 1 || discs > 10) {
+			System.out.println("Please enter between 1 - 10");
 		}
 		input.close();
 		
@@ -91,8 +54,6 @@ public class Towers {
 
 		printStacks();
 		solveTOH(discs, A, B, C);
-		while (C.size() > 0){
-			C.pop();
-		}
+		System.out.println("Nums of move " + count);
 	}
 }
